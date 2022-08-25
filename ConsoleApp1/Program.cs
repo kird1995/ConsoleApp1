751302c0ef08d9757eb9b1607e8523af6ec4673f
Владимир Александрович Кирдяшкин <kva@vast.su>
2022-08-25 18:10:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+       {
+        static void Main(string[] args) {
+         
+        double y;
+        double x;
+        double z;
+        double k;
+        y = GetNum("y");
+        x = GetNum("x");
+        z = GetNum("z");
+        k = GetNum("k")
+            GetAnswer(y, x, z, k)
+            Console.ReadKey();
+        }
+            static void GetAnswer(double y, double x, double z, double k) { 
+            
+            
+        }
+       
+    }
+   
+}
+
